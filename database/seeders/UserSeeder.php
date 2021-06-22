1a5fa5f217f0daa93ad6df5c0d0b8e16bd7d5e19
張 建文 <chang180@gmail.com>
2021-06-22 12:08:00 +0800

    add admin account field and seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => bcrypt('12341234'), 'is_admin' => 1]);
+    }
+}
