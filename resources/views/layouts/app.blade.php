2ecb0f33e41df873cda076e2e599bf14e5137f8e
張 建文 <chang180@gmail.com>
2021-06-24 14:28:52 +0800

    update user list and implement jQuery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,6 +69,7 @@
 
         <!-- Optional JavaScript -->
         <!-- Popper.js first, then CoreUI JS -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.1/perfect-scrollbar.min.js"
                 integrity="sha512-yUNtg0k40IvRQNR20bJ4oH6QeQ/mgs9Lsa6V+3qxTj58u2r+JiAYOhOW0o+ijuMmqCtCEg7LZRA+T4t84/ayVA=="
                 crossorigin="anonymous" referrerpolicy="no-referrer"></script>
